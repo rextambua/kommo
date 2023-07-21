69dfab1511025ad725f52fa51cd62076804300a5
rextambua <rextambua1@gmail.com>
2023-07-21 04:27:09 +0300

    kommos test

---
+++ Desktop/komm/1 task/index.js
@@ -0,0 +1,49 @@
+const inputEl = document.querySelector('input');
+const buttonEl = document.querySelector('button');
+const timerEl = document.querySelector('span');
+
+// Напишите реализацию createTimerAnimator
+// который будет анимировать timerEl
+const formatTime = (hours, minutes, seconds) => {
+  return `${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
+};
+
+const createTimerAnimator = () => {
+  let intervalId;
+  return (seconds) => {
+    let remainingSeconds = seconds;
+
+    const updateTimer = () => {
+      const hours = Math.floor(remainingSeconds / 3600);
+      const minutes = Math.floor((remainingSeconds % 3600) / 60);
+      const seconds = remainingSeconds % 60;
+
+      timerEl.textContent = formatTime(hours, minutes, seconds);
+
+      if (remainingSeconds <= 0) {
+        clearInterval(intervalId);
+      } else {
+        remainingSeconds--;
+      }
+    };
+
+    updateTimer();
+    intervalId = setInterval(updateTimer, 1000);
+  };
+};
+
+const animateTimer = createTimerAnimator();
+
+inputEl.addEventListener('input', () => {
+  // Очистите input так, чтобы в значении
+  // оставались только числа
+  inputEl.value = inputEl.value.replace(/\D/g, '');
+});
+
+buttonEl.addEventListener('click', () => {
+  const seconds = Number(inputEl.value);
+
+  animateTimer(seconds);
+
+  inputEl.value = '';
+});
